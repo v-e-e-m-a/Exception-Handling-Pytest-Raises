56c2d23402780e2331ad13504debcbb7d1a45800
Veema Jhagru <veemajhagru@gmail.com>
2025-09-17 14:18:48 -0400

    Implemented test_invalid_name_no_pytest function

--- tests/test_school_name_no_pytest_raises.py
+++ tests/test_school_name_no_pytest_raises.py
@@ -7,6 +7,13 @@ def test_invalid_name_no_pytest():
     #     claim_unreserved_code_school_name("Ada Developers Academy")
 
     # Think about what the test actually does and think about how to write them ourselves.
+    try:
+        school_name = 'Ada Developers Academy'
+        claim_unreserved_code_school_name(school_name)
+        assert False
+    except ValueError as error:
+        assert "There's already an awesome school with that name!"
+    
 
     # Replace this with your own logic
-    raise Exception("test not implemented")
+    #raise Exception("test not implemented")
